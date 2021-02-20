b43e601b53e56a9abc159a58a2b217218948b345
auveekan <anindya.auveek@gmail.com>
2021-02-20 00:16:25 -0500

    Match Function

---
+++ Matcher.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+Chosen = np.array([])   # 1010 [People] by 42[Categories] Numpy Array, where last two columns are Username and Password
+
+
+def find_matches(user_data, num):
+    '''
+    :param user_data: Row from Array
+    :param num: Number of matches to find
+    :return: UserIds of Matches
+    '''
+
+    diff = list(np.argsort([np.linalg.norm(Chosen[i, :38] - user_data[:38], ) for i in range(1010)]))
+    return [Chosen[diff.index(i)][40] for i in range(1,num+1)]
